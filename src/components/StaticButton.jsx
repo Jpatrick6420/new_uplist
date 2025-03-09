c11bd3aa1501a715e78b5b7da400ff912d6a944f
Justin Patrick <jpatrick6420@gmail.com>
2025-03-09 09:25:08 -0600

    changed styling

--- src/components/StaticButton.jsx
+++ src/components/StaticButton.jsx
@@ -1,7 +1,13 @@
 /* eslint-disable react/prop-types */
 
 function StaticButton({ label, handleClick, color }) {
-  const buttonClass = `bg-${color}-500 hover:bg-${color}-300 px-1 py-0.5 text-stone-100 w-full`;
+  const colorClasses = {
+    red: "bg-red-500 hover:bg-red-400",
+    violet: "bg-violet-500 hover:bg-violet-400",
+    emerald: "bg-emerald-500 hover:bg-emerald-400",
+  };
+
+  const buttonClass = `px-1 py-0.5 text-stone-100 w-full ${colorClasses[color]}`;
   return (
     <div className="w-full">
       <button onClick={handleClick} className={buttonClass}>
