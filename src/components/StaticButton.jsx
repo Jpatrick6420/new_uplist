a2cd46bc48ebeddfbf6e7e7f65d87ec94d9ec255
Justin Patrick <jpatrick6420@gmail.com>
2025-03-09 09:17:01 -0600

    added buttonclass to static button

--- src/components/StaticButton.jsx
+++ src/components/StaticButton.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable react/prop-types */
 
 function StaticButton({ label, handleClick, color }) {
+  const buttonClass = `bg-${color}-500 hover:bg-${color}-300 px-1 py-0.5 text-stone-100 w-full`;
   return (
     <div className="w-full">
-      <button
-        onClick={handleClick}
-        className={`bg-${color}-500 hover:bg-${color}-300 px-1 py-0.5 text-stone-100 w-full`}
-      >
+      <button onClick={handleClick} className={buttonClass}>
         {label}
       </button>
     </div>
